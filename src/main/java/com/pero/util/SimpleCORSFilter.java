89b07522d6bc710e25733144f0dc88f378b08ff4
progulj <progulj@gmail.com>
2015-05-31 16:47:35 +0200

    access contol

--- src/main/java/com/pero/util/SimpleCORSFilter.java
+++ src/main/java/com/pero/util/SimpleCORSFilter.java
@@ -17,9 +17,10 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET,PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers",   "Content-Type, Accept, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers",   "Origin, X-Requested-With, Content-Type, Accept");
+        
         chain.doFilter(req, res);
     }
 
